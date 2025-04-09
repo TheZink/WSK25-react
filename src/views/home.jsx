9f2413051af9ac2ecadc223f71ee687ca47a91aa
TheZink <ilkka.sinkonen@gmail.com>
2025-04-09 11:18:16 +0300

    Update getMedia to use real api endpoint

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     const getMedia = async () => {
 
       try {
-        const data = await fetchData('test.json');
+        const data = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
         setMediaArray(data);
       } catch (error){
         console.error('error', error);
