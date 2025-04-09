28b5f8d9ebc28cc5da526b6cafe1d187b805ea41
TheZink <ilkka.sinkonen@gmail.com>
2025-04-09 12:40:12 +0300

    Edited readme

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~ilkkasin/WSK25/week4/routing/',
+  base: '/~ilkkasin/WSK25/week4/hooks/',
 });
