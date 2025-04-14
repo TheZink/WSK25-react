d7e7fade8f0f16b01d91b2e9b7e814ffc4683273
TheZink <ilkka.sinkonen@gmail.com>
2025-04-14 10:29:25 +0300

    Fixed url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~ilkkasin/WSK25/week4/hooks/',
+  base: '/~ilkkasin/WSK25/week5/custom-hooks/',
 });
